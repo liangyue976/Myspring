88b02240c54a65bc2b5065c9c1a54f6ec3397323
caidongjing <meet_liangyue@163.com>
2020-12-06 15:06:41 +0800

    测试 1 第二次提交

--- liangyue976myspring/src/main/java/liangyue976/eureka/gittest.java
+++ liangyue976myspring/src/main/java/liangyue976/eureka/gittest.java
@@ -3,5 +3,6 @@ package liangyue976.eureka;
 public class gittest {
     public static void main(String[] args) {
         System.out.println("git 提交测试测试");
+        System.out.println("git 1 提交的代码");
     }
 }
